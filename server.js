ad51fb428c2549819c1efe287ea6f0dee5534bb5
malavika2007 <lajivv@gmail.com>
2022-02-02 23:11:53 +0530

    package  json  changed

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/rmsWeb'));
+app.use(express.static(__dirname + '/dist/rms-web'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/rmsWeb/index.html'));
+res.sendFile(path.join(__dirname+'/dist/rms-web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
